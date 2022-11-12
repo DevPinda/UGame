d60b995e0eb112d8694b33d49d069cb47867a778
Victorymp <44678273+Victorymp@users.noreply.github.com>
2022-11-12 02:38:03 +0000

    V 0.1
    - Added new routes to connect the database pages
    - Added creation and insertion code for the database
    - ord_create.blade.php is some PHP which should be moved and finished for the basket
       - all the order columns must line up with the HTML input values

---
+++ ugame-laravel-backend/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PageController::class,'index']);
+
+Route::get('/index', [PageController::class,'index']);
+
+Route::get('/nintendo', [PageController::class,'nintendo']);
+
+Route::get('/basket', [PageController::class,'basket']);
+
+Route::get('/xbox', [PageController::class,'xbox']);
+
+Route::get('/playstation', [PageController::class,'playstation']);
+
+Route::get('/signin', [PageController::class,'signin']);
+
+Route::get('/deals', [PageController::class,'deals']);
+
+Route::get('/register', [PageController::class,'register']);
+
+Route::get('/order', [PageController::class, 'insertform']);
+
+Route::post('/create', [PageController::class, 'order_insert']);
+
+Route::post('/order_table_Create', [PageController:: class, 'order_create_table']);
+
