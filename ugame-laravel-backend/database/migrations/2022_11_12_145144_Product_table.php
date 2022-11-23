ec31c6bb705ba16310c74f411a0921e3b8ffb3bc
Victory Mpokosa <mpokosa5@gmail.com>
2022-11-23 11:14:38 +0000

    Update 2022_11_12_145144_Product_table.php

--- ugame-laravel-backend/database/migrations/2022_11_12_145144_Product_table.php
+++ ugame-laravel-backend/database/migrations/2022_11_12_145144_Product_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
                     $table->integer('orders');
                     $table->integer('players');
                     $table->float('price');
-                    $table->integer('rating')
+                    $table->integer('rating');
             }
 
         );}
