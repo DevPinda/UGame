ff40a1fd155340a84e79c5ee02ab006fdaa2862d
Tyler <t.gilkes217@gmail.com>
2022-11-25 16:47:22 +0000

    Added links for 'About' and 'Contact' page.

--- ugame-laravel-backend/resources/views/index.blade.php
+++ ugame-laravel-backend/resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <script defer src="js/search.js"></script>
     <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/40ad289a3a.js" crossorigin="anonymous"></script>
@@ -19,17 +19,29 @@
 <body class = "all">
     <header>
         <div class = "slogan">
-            <h6 id = "phrase"> <marquee width="100%" text-align='center' > <img class = "white_logo" src = "images/UGAME logo (white).png" width = "100" height = "25"> Discover our latest releases.</marquee></h6>
+            <h6 id = "phrase"> <!--<marquee width="130%" text-align='center' >--> <img class = "white_logo" src = "images/UGAME logo (white).png" width = "100" height = "25"> Discover our latest releases.<!--</marquee>--></h6>
         </div>
+
+        <div id = "about_contact">
+                <ul id = "about_contact_txt">
+                    <li><a id="about_us" href="about">ABOUT</a></li>
+                    <li><a id="contact" href="contact">CONTACT</a></li>
+                </ul>
+            </div>
+
         <div class = "top_section">
+
             <a class = "logo" href="index"> <img class = "logo" src = "images/UGAME logo.png" width = "200" height = "50"> </a>
             <input id = search type="text" placeholder="Search games..." name="search"> 
             <button id = "search_button" type="button">Go</button>
+
+
             <ul id=register_basket>
                 <li><a id="sign_register" href="register"> <i class="fa-solid fa-user-large"></i> Sign in or Register </a></li>
                 <li><a id="basket" href="basket"> <i class="fa-solid fa-basket-shopping"></i> Basket</a></li>
             </ul>
         </div>
+
         <div class="nav_bar">
             <nav>
                 <ul id=links>
