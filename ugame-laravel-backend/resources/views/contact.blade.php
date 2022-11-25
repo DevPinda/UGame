2fe8c903fd29def6358592a130e141e20250c0f7
Tyler <t.gilkes217@gmail.com>
2022-11-25 16:44:34 +0000

    Set up new 'about.blade.php' and 'contact.blade.php' Shingai and Shari, you can now start working on these pages. I've commented down where you should start writing your HTML code.

---
+++ ugame-laravel-backend/resources/views/contact.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <script defer src="js/script.js"></script>
+    <script defer src="js/filter.js"></script>
+    <script defer src="js/search.js"></script>
+    
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/40ad289a3a.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+    <title>UGame?</title>
+</head>
+
+<div class = "all">
+    <body>
+    <header>
+        <div class = "slogan">
+            <h6 id = "phrase"> <!--<marquee width="130%" text-align='center' >--> <img class = "white_logo" src = "images/UGAME logo (white).png" width = "100" height = "25"> Discover our latest releases.<!--</marquee>--></h6>
+        </div>
+
+        <div id = "about_contact">
+                <ul id = "about_contact_txt">
+                    <li><a id="about_us" href="about">ABOUT</a></li>
+                    <li><a id="contact" href="contact">CONTACT</a></li>
+                </ul>
+            </div>
+
+        <div class = "top_section">
+
+            <a class = "logo" href="index"> <img class = "logo" src = "images/UGAME logo.png" width = "200" height = "50"> </a>
+            <input id = search type="text" placeholder="Search games..." name="search"> 
+            <button id = "search_button" type="button">Go</button>
+
+
+            <ul id=register_basket>
+                <li><a id="sign_register" href="register"> <i class="fa-solid fa-user-large"></i> Sign in or Register </a></li>
+                <li><a id="basket" href="basket"> <i class="fa-solid fa-basket-shopping"></i> Basket</a></li>
+            </ul>
+        </div>
+
+        <div class="nav_bar">
+            <nav>
+                <ul id=links>
+                    <li><a id="playstation" href="playstation">PLAYSTATION</a></li>
+                    <li><a id="xbox" href="xbox">XBOX</a></li>
+                    <li><a id="nintendo" href="nintendo">NINTENDO</a></li>
+                    <li><a id="deals" href="deals">DEALS</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <!--START HTML FOR ABOUT PAGE HERE SHARI:>-->
+
+    <body>
+        
+</div>
