087ab09444f8db6026d4d8496ff98e53faaa15ca
shinga1 <shingai41@outlook.com>
2022-11-27 17:17:58 +0000

    aboutUs (first commit)

---
+++ ugame-laravel-backend/resources/views/aboutUs.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <script defer src="js/script.js"></script>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/40ad289a3a.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+    <title>UGame?</title>
+</head>
+
+<body>
+    <header>
+        <div class = "slogan">
+            <h6 id = "phrase"> <img class = "white_logo" src = "images/UGAME logo (white).png" width = "100" height = "25"> Discover our latest releases.</h6>
+        </div>
+        <div class = "top_section">
+            <a class = "logo" href="index"> <img class = "logo" src = "images/UGAME logo.png" width = "200" height = "50"> </a>
+            <input id = search type="text" placeholder="Search games..." name="search"> 
+            <button id = "search_button" type="button">Go</button>
+            <ul id=register_basket>
+                <li><a id="sign_register" href="register"> <i class="fa-solid fa-user-large"></i> Sign in or Register </a></li>
+                <li><a id="basket" href="basket"> <i class="fa-solid fa-basket-shopping"></i> Basket</a></li>
+            </ul>
+        </div>
+        <div class="nav_bar">
+            <nav>
+                <ul id=links>
+                    <li><a id="playstation" href="playstation">PLAYSTATION</a></li>
+                    <li><a id="xbox" href="xbox">XBOX</a></li>
+                    <li><a id="nintendo" href="nintendo">NINTENDO</a></li>
+                    <li><a id="deals" href="deals">DEALS</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+        <section id="Get to know us">
+            <h1>UGame?</h1>
+            <img src="" width = "200" height = "50">
+            
+            <h2>Get to know us</h2>
+            <p>
+                We are a new found company that priorites on selling the latest, old school, 
+                classic and the trendiest games on varies platforms. Founded in 2022, our team are ready to
+                bring a new experience for each customer. Making it easier to purchase games           
+            </p>
+
+            <h2>Cookies</h2>
+            <p>Our e-commerce uses cookies which are a tiny text file that a website can place on your computer when you visit it. 
+                Cookies are used to improve your experience while using the website by facilitating navigation. 
+                Cookies allow a website to customise the content you see based on your browsing choices.
+            </p>
+            <p>  
+                No harm is brought to your computer or device when cookies are placed on your computer.
+            </p>
+
+            <h2> Shopping With Confidence </h2>
+            <p>
+                Rest easy knowing that we have a team that is dedicated to making sure that your experience at our safe is
+                is safe for every user. With experts that have years of experience within the cybersecurity, all transactions
+                are secure and kept confidential. Our website will always be active at any time of the day.
+            </p>
+
+    </header>
+
+</body>
+ 
