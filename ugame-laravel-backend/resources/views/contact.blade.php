370d34223e7c67913d716caba3e6b81f4aa9e53f
Tyler <t.gilkes217@gmail.com>
2022-11-29 12:33:58 +0000

    Contact page is complete (contact.blade.php).

--- ugame-laravel-backend/resources/views/contact.blade.php
+++ ugame-laravel-backend/resources/views/contact.blade.php
@@ -16,6 +16,19 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
     <title>UGame?</title>
+
+    <script>
+        function del_item(e){
+            let items = []
+            JSON.parse(localStorage.getItem('game_items')).map(data =>{
+                if (data.id != e.parentElement.parentElement.children[0].textContent){
+                    items.push(data);
+                }
+            });
+            localStorage.setItem('game_items', JSON.stringify(items));
+            window.location.reload();
+        }
+    </script>
 </head>
 
 <div class = "all">
@@ -29,6 +42,7 @@
                 <ul id = "about_contact_txt">
                     <li><a id="about_us" href="about">ABOUT</a></li>
                     <li><a id="contact" href="contact">CONTACT</a></li>
+                    <li><a id="admin" href="admin">ADMIN</a></li>
                 </ul>
             </div>
 
@@ -40,25 +54,49 @@
 
 
             <ul id=register_basket>
-                <li><a id="sign_register" href="register"> <i class="fa-solid fa-user-large"></i> Sign in or Register </a></li>
-                <li><a id="basket" href="basket"> <i class="fa-solid fa-basket-shopping"></i> Basket</a></li>
+                <li><a id="sign_register" href="register"> Sign in or Register </a></li>
+                <!--<li><a class="basket" id = "basket_link" href="basket"><i class="fa-solid fa-basket-shopping"></i> Basket</a></li>-->
+                <button id = "basket_link" type="button">Basket</button><p id = "item_num_noti">0</p>
             </ul>
         </div>
 
         <div class="nav_bar">
             <nav>
                 <ul id=links>
+                    <li><a id="deals" href="index">HOME</a></li>
                     <li><a id="playstation" href="playstation">PLAYSTATION</a></li>
                     <li><a id="xbox" href="xbox">XBOX</a></li>
                     <li><a id="nintendo" href="nintendo">NINTENDO</a></li>
-                    <li><a id="deals" href="deals">DEALS</a></li>
                 </ul>
             </nav>
         </div>
     </header>
 
-    <!--START HTML FOR ABOUT PAGE HERE SHARI:>-->
+    <section id="about_desc">
+            <div class = "each_sect">
+                <h2 class = "headings">Get in Touch</h2>
+                <p class = "paragraph">
+                    Phone: 1-800-883-8895<br>
+                    Email: care@ugame.com<br>    
+                    Address: Aston St, Birmingham B4 7ET   
+                </p>
 
-    <body>
-        
+                <p class = "paragraph">
+                    If you have any questions or queries please telephone or send us an email. 
+                    It might take us a little longer than usual to reply to emails but we will aim to reply within 5 working days.
+                </p>
+            </div>
+    </section>
+    <div id = "cart_window">
+        <div class = "cart_sect">
+            <button id = "close_button">Close Basket</button>
+            <h1 id = "basket_title">Your basket</h1>
+            <h1 id = "total_title">Subtotal: </h1>
+            <table class = "basket_table_"></table>
+            <button id = "checkout_button">Checkout</button>
+        </div>
+    </div>
+    <script src="js/script.js"></script>
+</div>
+<body>  
 </div>
