fbec622b422919c0c3de988938b61b20600f2a59
Victory Mpokosa <mpokosa5@gmail.com>
2022-11-29 00:28:28 +0000

    Bug Fixes for displaying different types of consoles
    This update makes changes to the (CS2TP-Group-27-Project-1\ugame-laravel-backend\app\Http\Controllers\ProductsController.php) to make it able to display different types of console types. As long as they have the console key word it can show any type of console.
    
    You will now be able to put any console in and the program will understand where to put it.

--- ugame-laravel-backend/app/Http/Controllers/ProductsController.php
+++ ugame-laravel-backend/app/Http/Controllers/ProductsController.php
@@ -6,22 +6,22 @@ use Illuminate\Support\Facades\DB;
 
 class ProductsController extends Controller {
     public function indexProducts(){
-        $indexProducts = DB::select('select * from products WHERE 1');
+        $indexProducts = DB::select('select * from products');
         return view('index', ['indexProducts'=>$indexProducts]);
     }
 
     public function playstationProducts(){
-        $playstationProducts = DB::select("select * from products WHERE platform='PlayStation' ");
+        $playstationProducts = DB::select("select * from products WHERE platform LIKE '%PlayStation%' ");
         return view('playstation', ['playstationProducts'=>$playstationProducts]);
     }
 
     public function xboxProducts(){
-        $xboxProducts = DB::select("select * from products WHERE platform='Xbox'");
+        $xboxProducts = DB::select("select * from products WHERE platform LIKE '%Xbox%'");
         return view('xbox', ['xboxProducts'=>$xboxProducts]);
     }
 
     public function nintendoProducts(){
-        $nintendoProducts = DB::select("select * from products WHERE platform='Nintendo'");
+        $nintendoProducts = DB::select("select * from products WHERE platform LIKE '%Nintendo%'");
         return view('nintendo', ['nintendoProducts'=>$nintendoProducts]);
     }
 }
